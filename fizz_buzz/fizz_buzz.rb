20cc082feca7b704041e05975f825b52c40559bd
Yue Wang <Moophyalan@gmail.com>
2015-07-11 03:43:59 +1200

    Update fizz_buzz.rb

--- fizz_buzz/fizz_buzz.rb
+++ fizz_buzz/fizz_buzz.rb
@@ -1,7 +1,7 @@
 class FizzBuzz
   def generate
     ret = ""
-    1.upto(100){ |n| ret += (n % 15 == 0 ? "FizzBuzz" : n % 5 == 0 ? "Buzz" : n % 3 == 0  ? "Fizz" : "#{n}") + "\n" }
-    ret
+    1.upto(100){ |n| ret += n % 15 == 0 ? "FizzBuzz" : n % 5 == 0 ? "Buzz" : n % 3 == 0  ? "Fizz" : "#{n}" }
+    ret + "\n"
   end
 end
