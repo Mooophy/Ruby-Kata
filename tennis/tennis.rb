f21f7dcccf8abe82fd3115e4dcacc058374c828d
Mooophy <MoophyAlan@gmail.com>
2015-07-11 03:46:56 +1200

    working 	modified:   tennis.rb

--- tennis/tennis.rb
+++ tennis/tennis.rb
@@ -1,58 +1,26 @@
-=begin
-class Player
-	
-	def initialize(name)
-		@name = name
-		@score = 0 	#in 0,1,2,3,4
-		@advantage = false
-	end
-
-	def toggle_advantage!
-		@advantage ^= true
-	end	
-
-end
-=end
 class Tennis
 
-  def initialize
+  def initialize(player1, player2)
+    @score_table = {player1 => 0, player2 => 0}
   end
-=begin
-	private
-	def initialize 
-		@home = 0
-		@away = 0
-		@score_table = { 0 => 0, 1 => 15, 2 => 30, 3 => 40, 4 => nil}
-		@score_table.freeze
-	end
 
-	def score(player)
-		@score_table[player] ? @score_table[player] : 40;
-	end
+  def score(player)
+    if @score_table[player]
+      if (0..2).include? @score_table[player]
+        @score_table[player] += 1
+      elsif @duel?
+        @score_table[player] = "advantage"
+        
+        
+      return true    
+    else
+      false   
+    end
+  end
 
-	public
-	def is_duel?
-		@home == 3 and @away == 3
-	end
-
-	def current_score
-		"#{ score(@home) },#{ score(@away) }"
-	end
-=end
-=begin
-	def initialize
-		@player_1_score = 0
-		@player_2_score = 0
-	end
-	def current_score
-		return "#{15 * @player_1_score},#{15 * @player_2_score}"
-	end
-	def player_1_score
-		@player_1_score += 1
-	end
-	def player_2_score
-		@player_2_score += 1
-	end
-=end
-
+  def duel?
+    result = true
+    @score_table.each_value {|value| result = false if value != 3}
+    result
+  end
 end
