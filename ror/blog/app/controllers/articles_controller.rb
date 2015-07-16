b453bda43a3fb25194a9cfa1f7c2a6f22c2275a8
yue <MoophyAlan@gmail.com>
2015-07-16 17:47:07 +1200

    working on 5.10

--- ror/blog/app/controllers/articles_controller.rb
+++ ror/blog/app/controllers/articles_controller.rb
@@ -1,11 +1,20 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
 
   def create
-    @article = Aticle.new(params[:article])
-
-    @article.save
-    redirect_to @article 
+    @article = Article.new(article_params)
+    
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
