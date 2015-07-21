a73230d079ea63ac190ce2b3d9f8906efd307f58
alan <moophyalan@gmail.com>
2015-07-21 14:03:31 +1200

    5.12 done

--- ror/blog/app/controllers/articles_controller.rb
+++ ror/blog/app/controllers/articles_controller.rb
@@ -11,6 +11,19 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @article = Article.new(article_params)
  
