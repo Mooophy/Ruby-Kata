7be6d6be4b08e8a2892155c56280a1cb504ce324
Yue Wang <moophyalan@gmail.com>
2015-07-27 16:39:02 +1200

    added so question link for the dynamic methods creation

--- test_activerecord/schema.rb
+++ test_activerecord/schema.rb
@@ -7,7 +7,7 @@ ActiveRecord::Schema.define do
   # create a table with 5 fields (actually 6 in total including id)
   create_table :cars do |t|               #where t is of TableDefinition
     t.string    :name,        limit: 255  # ? = where the string method or something else come from? 
-    t.integer   :year,        limit: 4
+    t.integer   :year,        limit: 4    # here: http://stackoverflow.com/questions/31644218/where-does-the-tabledefinition-string-method-come-from
     t.integer   :owner_id,    limit: 4
     t.decimal   :price,       precision: 8, scale: 2
     t.datetime  :purchased_at
